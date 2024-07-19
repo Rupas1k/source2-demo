410a1d116939ec9ba801c438215d7142867fdbff
Rupas1k <artem-rupasov@ya.ru>
2024-07-18 21:41:47 -0700

    Allow message reference

--- d2-stampede-macros/src/lib.rs
+++ d2-stampede-macros/src/lib.rs
@@ -3,7 +3,7 @@ mod protobuf_map;
 use crate::protobuf_map::get_enum_from_struct;
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, ItemImpl, Type};
+use syn::{parse_macro_input, ItemImpl, Type, FnArg};
 
 #[proc_macro_attribute]
 pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -24,17 +24,25 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
     for item in &input.items {
         if let syn::ImplItem::Fn(method) = item {
             let method_name = &method.sig.ident;
+
+            check_second_arg_is_context(method);
+            
             for attr in &method.attrs {
                 if attr.path().is_ident("on_message") {
-                    if let Some(arg_type) = get_message_type(method) {
+                    if let Some((arg_type, is_reference)) = get_message_type(method) {
                         let enum_type = get_enum_from_struct(&arg_type.to_token_stream().to_string());
+                        let call_message = if is_reference {
+                            quote! { self.#method_name(ctx, &message)?; }
+                        } else {
+                            quote! { self.#method_name(ctx, message)?; }
+                        };
                         match enum_type.to_token_stream().to_string().split("::").collect::<Vec<_>>()[0].trim() {
                             "EDotaUserMessages" => {
                                 on_dota_user_message_body = quote! {
                                     #on_dota_user_message_body
                                     if msg_type == #enum_type {
                                         if let Ok(message) = #arg_type::decode(msg) {
-                                            self.#method_name(ctx, message)?;
+                                            #call_message
                                         }
                                     }
                                 };
@@ -44,7 +52,7 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                                     #on_base_user_message_body
                                     if msg_type == #enum_type {
                                         if let Ok(message) = #arg_type::decode(msg) {
-                                            self.#method_name(ctx, message)?;
+                                            #call_message
                                         }
                                     }
                                 };
@@ -54,7 +62,7 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                                     #on_svc_message_body
                                     if msg_type == #enum_type {
                                         if let Ok(message) = #arg_type::decode(msg) {
-                                            self.#method_name(ctx, message)?;
+                                            #call_message
                                         }
                                     }
                                 };
@@ -64,7 +72,7 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                                     #on_base_game_event_body
                                     if msg_type == #enum_type {
                                         if let Ok(message) = #arg_type::decode(msg) {
-                                            self.#method_name(ctx, message)?;
+                                            #call_message
                                         }
                                     }
                                 };
@@ -74,7 +82,7 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                                     #on_net_message_body
                                     if msg_type == #enum_type {
                                         if let Ok(message) = #arg_type::decode(msg) {
-                                            self.#method_name(ctx, message)?;
+                                            #call_message
                                         }
                                     }
                                 };
@@ -84,13 +92,15 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                                     #on_demo_command_body
                                     if msg_type == #enum_type {
                                         if let Ok(message) = #arg_type::decode(msg) {
-                                            self.#method_name(ctx, message)?;
+                                            #call_message
                                         }
                                     }
                                 };
                             }
                             x => unreachable!("{}", x),
                         }
+                    } else {
+                        panic!("Message handler must have a message argument")
                     }
                 }
 
@@ -107,7 +117,6 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                         self.#method_name(ctx)?;
                     };
                 }
-
 
                 if attr.path().is_ident("on_entity") {
                     on_entity_body = quote! {
@@ -203,7 +212,7 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 #on_tick_end_body
                 Ok(())
             }
-            
+
             fn on_entity(
                 &mut self,
                 ctx: &Context,
@@ -213,7 +222,7 @@ pub fn observer(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 #on_entity_body
                 Ok(())
             }
-            
+
             fn on_combat_log(
                 &mut self,
                 ctx: &Context,
@@ -255,6 +264,36 @@ pub fn on_combat_log(_attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
 
-fn get_message_type(method: &syn::ImplItemFn) -> Option<&Type> {
-    method.sig.inputs.iter().nth(2).and_then(|arg| if let syn::FnArg::Typed(pat_type) = arg { Some(&*pat_type.ty) } else { None })
+fn get_message_type(method: &syn::ImplItemFn) -> Option<(Type, bool)> {
+    method.sig.inputs.iter().nth(2).and_then(|arg| {
+        if let syn::FnArg::Typed(pat_type) = arg {
+            if let Type::Reference(x) = pat_type.ty.as_ref() {
+                if x.mutability.is_some() {
+                    panic!("Mutable reference not supported")
+                }
+                Some((*x.elem.clone(), true))
+            } else {
+                Some((*pat_type.ty.clone(), false))
+            }
+        } else {
+            None
+        }
+    })
+}
+
+
+fn check_second_arg_is_context(method: &syn::ImplItemFn) {
+    if let Some(FnArg::Typed(pat_type)) = method.sig.inputs.iter().nth(1) {
+        if let Type::Reference(type_reference) = pat_type.ty.as_ref() {
+            let elem_type = type_reference.elem.as_ref();
+            if let Type::Path(type_path) = elem_type {
+                if let Some(segment) = type_path.path.segments.first() {
+                    if segment.ident == "Context" && type_reference.mutability.is_none() {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    panic!("The second argument must be of type &Context");
 }
